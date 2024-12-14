ced3e7c746332b3736131db0ff3f282d6c606914
ZennoZenith <ZennoZenith@proton.me>
2024-12-14 09:49:21 +0530

    feat: removed unneeded css files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 import "../app.css";
-import "../base.css";
 import Toaster from "$lib/components/Toaster.svelte";
 import ToastTest from "$lib/components/ToastTest.svelte";
 import { setToastState } from "$lib/toast-state.svelte";
