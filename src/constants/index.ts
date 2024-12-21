9297d86ccd3a165d32f27f48281254463d07b182
ZennoZenith <ZennoZenith@proton.me>
2024-12-21 10:23:42 +0530

    fix: using public svelte env as constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,9 @@
 /*
   only declare fields which are public accessible
 */
-export const API_BASE_ROUTE = "http://localhost:8081/api";
-export const IMAGE_BASE_ROUTE = "http://192.168.1.20:8082";
+import { env } from "$env/dynamic/public";
+
+export const API_BASE_ROUTE = env.PUBLIC_API_BASE_ROUTE ?? "http://localhost:8081/api";
+export const IMAGE_BASE_ROUTE = env.PUBLIC_IMAGE_BASE_ROUTE ?? "http://192.168.1.20:8082";
 export const DEFAULT_TOAST_DURATION_TIME = 5000; // in milliseconds
-export const isDevelopment = process.env.NODE_ENV === "development";
+export const isDevelopment = env.PUBLIC_NODE_ENV === "development";
