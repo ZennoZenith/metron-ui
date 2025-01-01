97027ea52ec8d213d18350e49c0c863b805dd494
ZennoZenith <ZennoZenith@proton.me>
2025-01-01 09:44:49 +0530

    fix: constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,6 @@
 import { env } from "$env/dynamic/public";
 
 export const API_BASE_ROUTE = env.PUBLIC_API_BASE_ROUTE ?? "http://localhost:10105/api";
-export const IMAGE_BASE_ROUTE = env.PUBLIC_IMAGE_BASE_ROUTE ?? "http://192.168.1.20:10109";
+export const IMAGE_BASE_ROUTE = env.PUBLIC_IMAGE_BASE_ROUTE ?? "http://localhost:10109";
 export const DEFAULT_TOAST_DURATION_TIME = 5000; // in milliseconds
 export const isDevelopment = env.PUBLIC_NODE_ENV === "development";
