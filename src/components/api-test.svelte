5e094bebcc4ddd37e5b207b43d5c7500bc9d160b
ZennoZenith <ZennoZenith@proton.me>
2025-02-12 16:30:04 +0530

    fix:

--- src/components/api-test.svelte
+++ src/components/api-test.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-import ImageApiClient from "$features/images/api";
+import { ImageApiClient } from "$features/images/api";
 import { apiClientOptions } from "$lib/api-builder";
 import { Log } from "$lib/logger";
 
 const imageClient = new ImageApiClient(apiClientOptions);
 
 (async () => {
-  const temp = await imageClient.getImageById(
+  const temp = await imageClient.getById(
     "0964cf80-c7f2-11ef-8a8e-ff84e6ecd36a",
   );
   Log.info(temp);
